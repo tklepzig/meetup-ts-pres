1bc139ec9fd4f2ff00c447cccf53e53d51e005ef
Sebastian Spiess <sebastian.spiess@invision.de>
2019-07-04 16:29:09 +0200

    Add exercise for type guards

---
+++ exercises/unionTypesAndTypeGuards/typeGuards.js
@@ -0,0 +1,9 @@
+function rollDice(num) {
+  if (num > 6) {
+    return NaN;
+  }
+  return num;
+}
+
+console.log(rollDice(5));
+console.log(rollDice(7));
---
+++ exercises/unionTypesAndTypeGuards/typeGuardsTs.ts
@@ -0,0 +1,6 @@
+const rollDice = (num: 1 | 2 | 3 | 4 | 5 | 6) => {
+  return num;
+};
+
+console.log(rollDice(5));
+console.log(rollDice(7));
